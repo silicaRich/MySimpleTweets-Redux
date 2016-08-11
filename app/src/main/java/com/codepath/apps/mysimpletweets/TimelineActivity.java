429967b5db5f42b9892db0179421091b0c0b0701
Nicole Flokos <nicole.flokos@gmail.com>
2016-08-11 01:32:47 -0400

    Swipe Left or Right :ghost:

--- app/src/main/java/com/codepath/apps/mysimpletweets/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/mysimpletweets/TimelineActivity.java
@@ -3,6 +3,10 @@ package com.codepath.apps.mysimpletweets;
 import android.content.ClipData;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -11,6 +15,8 @@ import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.codepath.apps.mysimpletweets.fragments.HomeTimelineFragment;
+import com.codepath.apps.mysimpletweets.fragments.MentionsTimelineFragment;
 import com.codepath.apps.mysimpletweets.fragments.TweetsListFragment;
 import com.codepath.apps.mysimpletweets.models.Tweet;
 import com.loopj.android.http.JsonHttpResponseHandler;
@@ -23,6 +29,21 @@ import java.util.ArrayList;
 import cz.msebera.android.httpclient.Header;
 
 public class TimelineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timeline);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        setSupportActionBar(toolbar);
+
+
+        ViewPager vpPager = (ViewPager) findViewById(R.id.viewpager);
+        vpPager.setAdapter(new TweetsPagerAdapter(getSupportFragmentManager()));
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -38,20 +59,46 @@ public class TimelineActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_timeline);
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        // Sets the Toolbar to act as the ActionBar for this Activity window.
-        setSupportActionBar(toolbar);
-    }
-
-    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
        // max_id = 1;
      //   tweets.clear();
      //   aTweets.notifyDataSetChanged();
      //   populateTimeline(1);
     }
+
+    public class TweetsPagerAdapter extends FragmentPagerAdapter{
+
+        private String tabTitles[] = {"Home", "Mentions"};
+
+        // Adapter gets teh manager insert of remove fragment from activity
+        public TweetsPagerAdapter(FragmentManager fm){
+            super(fm);
+        }
+
+        // Order and creation of fragments within the pager
+        @Override
+        public Fragment getItem(int position) {
+            if (position == 0){
+                return new HomeTimelineFragment();
+            }
+            else if (position ==1){
+                return new MentionsTimelineFragment();
+            }
+            else
+                return null;
+        }
+
+        // Return Tab Title
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return tabTitles[position];
+        }
+
+        // Returns number of tabs
+        @Override
+        public int getCount() {
+            return tabTitles.length;
+        }
+    }
+
 }
